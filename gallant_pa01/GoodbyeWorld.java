405c3a53c24946781289f09ffeb12240adf7d73a
DarthofLuke <gallant.thomas@outlook.com>
2020-01-17 22:08:46 -0500

    Initial commit

---
+++ gallant_pa01/GoodbyeWorld.java
@@ -0,0 +1,10 @@
+package gallant_pa01;
+
+public class GoodbyeWorld{
+
+	public static void main(String[] args) {
+		System.out.println("Goodbye, World!");
+
+	}
+
+}
---
+++ gallant_pa01/HelloWorld.java
@@ -0,0 +1,11 @@
+package gallant_pa01;
+ 
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+
+	}
+
+}
